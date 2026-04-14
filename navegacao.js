71ef7fc01acc8f29454c243f796529afa4f884f8
IgrejaManancialdeVida <igrejamanancialdevidaemcampos@gmail.com>
2026-04-14 00:34:45 -0300

    Update navegacao.js

--- navegacao.js
+++ navegacao.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // 2. DEFINA OS LINKS REAIS AQUI (Substitua pelos seus links do GitHub)
     const volumes = [
-        { nome: 'Volume I', link: 'https://github.com/IgrejaManancialdeVida/li-esebd-professores/' },
+        { nome: 'Volume I', link: 'https://igrejamanancialdevida.github.io/li-esebd-professores/' },
         { nome: 'Volume II', link: 'https://github.com/IgrejaManancialdeVida/li-esebd-professores-v.2-/' },
         { nome: 'Volume III', link: 'https://github.com/IgrejaManancialdeVida/li-esebd-professores-v.3/' }
     ];
